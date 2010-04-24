694fd5f333fd51e56a53f1dc896aa374c4f60e4d
janripke <34332389+janripke@users.noreply.github.com>
2010-04-24 23:24:42 +0000

    remove deprecated scripts. added readme.txt

---
+++ src/examples/helloworld-db/create/apps/ddl/seq/application_properties_s.sql
@@ -0,0 +1,6 @@
+create sequence APPLICATION_PROPERTIES_S
+minvalue 1
+maxvalue 999999999999
+start with 1
+increment by 1
+nocache;
---
+++ src/examples/helloworld-db/create/apps/ddl/trg/application_properties_biu.sql
@@ -0,0 +1,16 @@
+create or replace trigger application_properties_biu
+  before insert or update on application_properties
+  for each row
+begin
+  if inserting then
+    if :new.id is null then
+      select application_properties_s.nextval 
+      into   :new.id 
+      from   dual;
+    end if;
+  elsif updating then
+    :new.updated_at:=sysdate;
+    :new.updated_by:=user;
+  end if;
+end application_properties_biu;
+/
