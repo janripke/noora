aa4072cf50dd52734dfc1db7c8a8d7d8deb403b1
janripke <34332389+janripke@users.noreply.github.com>
2010-11-15 16:44:39 +0000

    changed drop for jar and schema

--- src/plugins/static/drop/jar/drop_jars.sql
+++ src/plugins/static/drop/jar/drop_jars.sql
@@ -1,15 +1,15 @@
 set serveroutput on
 declare
   cursor c_user_java_classes is
-    select source
-    from user_java_classes;
+    select object_name
+    from user_objects where object_type='JAVA SOURCE';
 
   statement	    varchar2(1024);
   M_DQUOTE      varchar2(1):='"';
-  
+
 begin
-  for user_java_class in c_user_java_classes loop      
-    statement:='DROP JAVA SOURCE ' || M_DQUOTE || user_java_class.source || M_DQUOTE; 
+  for user_java_class in c_user_java_classes loop
+    statement:='DROP JAVA SOURCE ' || M_DQUOTE || user_java_class.object_name || M_DQUOTE;
     execute immediate statement;
   end loop;
 end;
