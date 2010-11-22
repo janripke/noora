a5ef95ee9b1f64155143e3bb7978fa59725b708c
peterkist <17298211+peterkist-tinker@users.noreply.github.com>
2010-11-22 23:24:25 +0000

    first start of gui for noora

---
+++ src/NooraGUI.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#Boa:App:NooraGUIApp
+
+import wx
+
+import TopFrame
+
+modules ={'TopFrame': [1, 'Main frame of the Application', u'TopFrame.py']}
+
+class NooraGuiApp(wx.App):
+    def OnInit(self):
+        self.main = TopFrame.create(None)
+        self.main.Show()
+        self.SetTopWindow(self.main)
+        return True
+
+def main():
+    application = NooraGuiApp(0)
+    application.MainLoop()
+
+if __name__ == '__main__':
+    main()
