4cd8274249a892f2120e78590e0133f55f92c4d8
janripke <janripke@b27cb4f7-efab-4f3b-aaa8-6348f188646f>
2012-03-22 11:17:29 +0000

    aq update

--- trunk/src/plugins/static/drop/aq/drop_queue.sql
+++ trunk/src/plugins/static/drop/aq/drop_queue.sql
@@ -11,11 +11,12 @@ begin
     statement:='begin dbms_aqadm.stop_queue(queue_name => '|| M_QUOTE || user_object.name || M_QUOTE || '); end;';
     execute immediate statement;
   
-    statement:='begin dbms_aqadm.drop_queue_table(queue_table => '|| M_QUOTE || user_object.queue_table || M_QUOTE||', force => true); end;';     
-    execute immediate statement;
-    
     statement:='begin dbms_aqadm.drop_queue(queue_name => '|| M_QUOTE || user_object.name || M_QUOTE || '); end;';     
     execute immediate statement;
+
+--    statement:='begin dbms_aqadm.drop_queue_table(queue_table => '|| M_QUOTE || user_object.queue_table || M_QUOTE||', force => true); end;';     
+--    execute immediate statement;
+    
   end loop;
 end;
 /
