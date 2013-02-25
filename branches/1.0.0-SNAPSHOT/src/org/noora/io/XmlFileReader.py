33c608ac402a744177a922d0f8776cbd5e60730a
peterkist <peterkist@b27cb4f7-efab-4f3b-aaa8-6348f188646f>
2013-02-25 22:41:54 +0000

    adding config package (xml based) and accompanying test

--- branches/1.0.0-SNAPSHOT/src/org/noora/io/XmlFileReader.py
+++ branches/1.0.0-SNAPSHOT/src/org/noora/io/XmlFileReader.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python
+
 from org.noora.io.Readable import Readable
+from xml.etree import ElementInclude
 from xml.etree.ElementTree import ElementTree
 
-
-
 class XmlFileReader(Readable):
-  def __init__(self, file=None):
+  def __init__(self, filename=None):
     Readable.__init__(self)
-    self.__file = file
-    pathName = file.getPath() + file.getName()
-    self.__handle = ElementTree()
-
-    
-    
-    
+    self.__file = filename
+    self.__treeroot = ElementTree()
+   
   def read(self):
-    handle = self.__handle
-    file = self.__file
-    handle.parse(file.getPath()+file.getName())    
+    handle = self.__treeroot
+    handle.parse(self.__file.getPathName())
+    root = handle.getroot()
+    ElementInclude.include(root)
     return handle
   
   def getFile(self):
     return self.__file
     
+  def getHandle(self):
+    return self.__treeroot
+  
   def close(self):
     pass
-    
-
-       
-
-
---
+++ branches/1.0.0-SNAPSHOT/test/config/TestXmlConfig.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import sys
+from org.noora.config.XmlConfig import XmlConfig
+
+BASE_DIR = os.path.abspath('.')
+NOORA_DIR = BASE_DIR.split('test')[0] + "src"
+
+sys.path.append(NOORA_DIR)
+
+from org.noora.io.File import File
+from org.noora.io.FileReader import FileReader
+from org.noora.io.XmlFileReader import XmlFileReader
+
+class TestXmlConfig(unittest.TestCase):
+  
+  def getFileReader(self, fileName):
+    file = File(fileName)
+    fileReader = XmlFileReader(file)
+    return fileReader
+  
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+      
+  def testXmlFileReader(self):
+    file = File("project-config.xml")
+    fileReader = XmlFileReader(file)
+    tree = fileReader.read()
+    fileReader.close()
+    
+    self.assertEqual(tree.findall("./name")[0].text,"ExampleProject","./name is invalid")
+    self.assertEqual(tree.findall("name")[0].text,"ExampleProject","name is invalid")
+
+  def testXmlConfig(self):
+    file = File("project-config.xml")
+    xmlConfig = XmlConfig(file)
+    xmlConfig.load();
+    
+    self.assertEqual(xmlConfig.getProperty("name"),"ExampleProject", "'name' property not found or invalid")
+    self.assertEqual(xmlConfig.getProperty("plugins/plugin[@name='generate']/class"),"dynamic.generate.GeneratePlugin.GeneratePlugin", "'plugin-class' property not found or invalid")
+
+
+if __name__ == '__main__':
+    unittest.main()
