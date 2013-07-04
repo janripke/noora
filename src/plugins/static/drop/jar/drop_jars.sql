774003eeadb52b9ea4e5e8ef9478c95b8c54809c
janripke <34332389+janripke@users.noreply.github.com>
2013-07-04 09:36:45 +0000

    fixed : loadjava, drop jars

--- src/plugins/static/drop/jar/drop_jars.sql
+++ src/plugins/static/drop/jar/drop_jars.sql
@@ -1,17 +1,27 @@
 set serveroutput on
 declare
-  cursor c_user_java_classes is
+  cursor c_user_java_sources is
     select object_name
     from user_objects where object_type='JAVA SOURCE';
 
+  cursor c_user_java_classes is
+    select object_name
+    from user_objects where object_type='JAVA CLASS';
+    
   statement	    varchar2(1024);
   M_DQUOTE      varchar2(1):='"';
 
 begin
-  for user_java_class in c_user_java_classes loop
-    statement:='DROP JAVA SOURCE ' || M_DQUOTE || user_java_class.object_name || M_DQUOTE;
+  for user_java_source in c_user_java_sources loop
+    statement:='DROP JAVA SOURCE ' || M_DQUOTE || user_java_source.object_name || M_DQUOTE;
     execute immediate statement;
   end loop;
+  
+  for user_java_class in c_user_java_classes loop
+    statement:='DROP JAVA CLASS ' || M_DQUOTE || user_java_class.object_name || M_DQUOTE;
+    execute immediate statement;
+  end loop;
+  
 end;
 /
 set serveroutput off
