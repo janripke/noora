54263848333a851b7455d788c083fe6ccfb501b5
janripke <janripke@b27cb4f7-efab-4f3b-aaa8-6348f188646f>
2013-12-07 21:53:28 +0000

    ticket #3, show help when no command is given

--- trunk/src/noora.py
+++ trunk/src/noora.py
@@ -11,21 +11,18 @@ import logging.handlers
 from org.noora.app.NoOraApp import NoOraApp
 
 __revision__ = "$Revision$"
-__version__  = "0.0.9"
+__version__  = "1.0.0-SNAPSHOT"
 
 NOORA_DIR    = os.path.abspath(os.path.dirname(sys.argv[0]))
-BASE_DIR     = os.path.abspath('.')
+BASE_DIR     = os.path.abspath('.') 
 PLUGIN_DIR   = NOORA_DIR+os.sep+'plugins'
 sys.path.append(PLUGIN_DIR)
 
 
 
-def getRevision():  
-  app = NoOraApp()
+def getRevision(app):  
   print "noora version '" + app.getVersion() + "'"
   print "noora runtime (build " + app.getVersion()  + "_" + app.getRevision().split(":")[1].rstrip("$").strip() + ")"
-  
-  
 
 def findTemplateFile(filename):
   url=BASE_DIR+os.sep+filename
@@ -50,12 +47,13 @@ if __name__ == "__main__":
     handler.setFormatter(formatter)
     logger.addHandler(handler)         
     handler.doRollover()        
-    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
     
+    app = NoOraApp()
     
     parameterHelper=ParameterHelper.ParameterHelper()      
     if parameterHelper.hasParameter('-r'):
-      getRevision()
+      getRevision(app)
       exit(1)
  
     config = ConfigReader.ConfigReader(findTemplateFile('project.conf'))
@@ -68,20 +66,27 @@ if __name__ == "__main__":
 
     parameters=parameterHelper.getParameters()
 
+    helpPlugin = classLoader.findByPattern('static.help.HelpPlugin.HelpPlugin')
+    pluginManager.registerPlugin(helpPlugin)
+
     plugins = config.getValue('PLUGINS')
     for plugin in plugins:
       pluginClass=classLoader.findByPattern(plugin)
       pluginManager.registerPlugin(pluginClass)
+
     
-        
+    if parameterHelper.hasParameter('-h'):
+      helpPlugin.execute(app, parameterHelper, pluginManager.getPlugins())
+      exit(1)    
+            
     for plugin in pluginManager.getPlugins():
-      
       for parameter in parameters:
-        if parameter.upper() == plugin.getType():
-          plugin.execute(parameterHelper)
+        if parameter.upper() == plugin.getType():       
+          plugin.execute(app,parameterHelper,pluginManager.getPlugins())
           exit(0)
 
-    failOnPluginNotFound('no plugin found.')
+    helpPlugin.execute(app, parameterHelper, pluginManager.getPlugins())
+    failOnPluginNotFound('no plugin found')
     
   
   except NooraException.NooraException as e:
--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='noora',
-    version='0.0.9',
+    version='1.0.0-SNAPSHOT',
     description='noora database project development.',
     author='Jan Ripke',
     author_email='janripke@gmail.com',
