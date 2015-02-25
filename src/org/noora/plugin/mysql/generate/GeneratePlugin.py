c1744e82d192621382f07f91bdeba1c39b7a4b55
janripke <34332389+janripke@users.noreply.github.com>
2015-02-25 22:04:53 +0000

    mysql generate plugin

--- src/org/noora/plugin/mysql/generate/GeneratePlugin.py
+++ src/org/noora/plugin/mysql/generate/GeneratePlugin.py
@@ -3,6 +3,7 @@ import os
 from org.noora.plugin.Plugin import Plugin
 from org.noora.helper.PropertyHelper import PropertyHelper
 from org.noora.io.File import File
+from org.noora.io.Files import Files
 from org.noora.io.Path import Path
 from org.noora.plugin.ConnectionExecutor import ConnectionExecutor
 from org.noora.connector.ExecuteFactory import ExecuteFactory
@@ -16,6 +17,7 @@ from core.Property import FileReader
 from org.noora.io.FileWriter import FileWriter
 from org.noora.io.Properties import Properties
 from org.noora.io.PropertyLoader import PropertyLoader
+from org.noora.version.VersionGuesser import VersionGuesser
 
 class GeneratePlugin(Plugin):
   
@@ -80,20 +82,24 @@ class GeneratePlugin(Plugin):
 
   def execute(self, commandLine, properties):
     
+    project = commandLine.getOptionValue('-pr')
+    version = commandLine.getOptionValue('-v')
+    
     configFile = File(Path.path(properties.getPropertyValue('current.dir'),properties.getPropertyValue('project.file')))
     if configFile.exists()==False:      
-      project = commandLine.getOptionValue('-pr')
+      #project = commandLine.getOptionValue('-pr')
       host = commandLine.getOptionValue('-h')
       database = commandLine.getOptionValue('-d')
       username = commandLine.getOptionValue('-u')
       password = commandLine.getOptionValue('-p')
-      version = commandLine.getOptionValue('-v')
+      #version = commandLine.getOptionValue('-v')
       
       # create the project folder
       os.makedirs(project)
       
 
       #create project.conf
+      templateFolder = Path.path(properties.getPropertyValue('plugin.dir'),'mysql','generate','templates')
       templateFile = File(Path.path(properties.getPropertyValue('plugin.dir'),'mysql','generate','templates',properties.getPropertyValue('project.file')))
       reader = FileReader(templateFile)
       stream = reader.read()
@@ -108,9 +114,9 @@ class GeneratePlugin(Plugin):
       writer.write(stream)
       writer.close()
     
-    alterDir = properties.getPropertyValue('alter.dir')
-    createDir = properties.getPropertyValue('create.dir')
-    
+    alterDir = Path.path(properties.getPropertyValue('current.dir'),project,'alter')
+    createDir = Path.path(properties.getPropertyValue('current.dir'),project,'create')
+   
       
     # load project.conf
     properties = Properties()        
@@ -131,5 +137,101 @@ class GeneratePlugin(Plugin):
     versionLoader.load(properties)
     versions.sort()  
     #print versions.list()
+    versionGuesser=VersionGuesser(properties, versions)
+    nextVersion = versionGuesser.guess(version).toString()
+    versionFolder = versionGuesser.toFolder(nextVersion)
     
+    databases = properties.getPropertyValues('DATABASES')
+    versionDatabase = properties.getPropertyValues('VERSION_DATABASE')
+    defaultVersion = properties.getPropertyValues('DEFAULT_VERSION')
+    environments = properties.getPropertyValues('ENVIRONMENTS')
+    objects= properties.getPropertyValues('CREATE_OBJECTS')
+    
+    # create the version folder
+    os.makedirs(versionFolder)
+    
+  
+    for database in databases:
+
+      # create the scheme folder
+      databaseFolder=versionFolder+os.sep+database
+      os.mkdir(databaseFolder)
+
+      # create the dat folder
+      datFolder=Path.path(databaseFolder,'dat')
+      os.mkdir(datFolder)
+
+      # create the version script in the dat folder
+      if database==versionDatabase:
+        versionFile = File(Path.path(datFolder,"version.sql"))
+        versionFileWriter = FileWriter(versionFile)
+        
+        if nextVersion == defaultVersion:
+          stream = properties.getPropertyValues('VERSION_INSERT_STATEMENT')          
+        else:
+          stream = properties.getPropertyValues('VERSION_UPDATE_STATEMENT')
+        
+        stream=stream.replace('<version>',nextVersion)               
+        
+        versionFileWriter.write(stream)
+        versionFileWriter.close()
+        
+        #sqlScript=self.getSqlVersionStatement(versions, version)
+        #projectHelper.writeFile(datFolder+os.sep+'version.sql', sqlScript)
+
+      # create the environment folders in the dat folder
+      for environment in environments:
+        
+        os.mkdir(Path.path(datFolder,environment))
+
+        # create the environment script in the dat folder.
+        if database==versionDatabase and nextVersion == defaultVersion:
+          environmentFile=File(Path.path(datFolder,environment,'environment.sql'))
+          environmentFileWriter = FileWriter(environmentFile)
+          stream = properties.getPropertyValues('ENVIRONMENT_INSERT_STATEMENT')
+          stream=stream.replace('<environment>',environment)
+
+          environmentFileWriter.write(stream)
+          environmentFileWriter.close()
+          
+          #sqlScript=self.getSqlEnvironmentStatement(environment)
+          #projectHelper.writeFile(datFolder+os.sep+environment+os.sep+'environment.sql', sqlScript)
+                
+
+      # create the ddl folder
+      ddlFolder=Path.path(databaseFolder,'ddl')
+      os.mkdir(ddlFolder)
+
+      # create the object folders in the ddl folder
+      for object in objects:
+        os.mkdir(Path.path(ddlFolder,object))
       
+      # create the template code on create.
+      if database==versionDatabase and nextVersion == defaultVersion:
+        for object in objects:
+          objectFolder = Path.path(templateFolder,object)
+          objectFile = File(objectFolder)
+          if objectFile.exists():
+            files = Files.list(objectFile)
+            for file in files:
+              fileReader = FileReader(file)
+              stream = fileReader.read()
+              fileReader.close()
+              
+              target = File(Path.path(ddlFolder,object,file.getName()))
+              targetWriter = FileWriter(target)
+              targetWriter.write(stream)
+              targetWriter.close()
+              
+      #  files=projectHelper.findFiles(folder)
+      #  for object in objects:
+      #    folder=self.getPluginDir()+os.sep+'templates'+os.sep+object
+      #    if projectHelper.folderPresent(folder):
+      #      files=projectHelper.findFiles(folder)
+      #      for file in files:
+      #        sourceFile=folder+os.sep+file
+      #        targetFile=ddlFolder+os.sep+object+os.sep+file
+      #        projectHelper.copyFile(sourceFile,targetFile)
+
+    print "version "+nextVersion+" created."
+    
--- test/plugins/mysql/generate/TestMysqlGeneratePlugin.py
+++ test/plugins/mysql/generate/TestMysqlGeneratePlugin.py
@@ -11,6 +11,7 @@ sys.path.append(NOORA_DIR)
 
 from org.noora.io.Properties import Properties
 from org.noora.io.PropertyLoader import PropertyLoader
+from org.noora.io.Path import Path
 from org.noora.io.File import File
 from org.noora.io.FileReader import FileReader
 from org.noora.plugin.mysql.generate.GeneratePlugin import GeneratePlugin
@@ -27,14 +28,14 @@ class TestBase(unittest.TestCase):
   def tearDown(self):
     pass
 
-  def testGenerateProjectPass(self):    
+  def testGenerateCreate(self):    
 
     properties = Properties()    
     properties.setProperty("noora.dir", NOORA_DIR)
-    properties.setProperty("plugin.dir", NOORA_DIR+os.sep+"org"+os.sep+"noora"+os.sep+"plugin")
+    properties.setProperty("plugin.dir", Path.path(NOORA_DIR,"org","noora","plugin"))
     properties.setProperty("current.dir", os.path.abspath('.'))
-    properties.setProperty("alter.dir",properties.getPropertyValue("current.dir")+os.sep+"alter")
-    properties.setProperty("create.dir",properties.getPropertyValue("current.dir")+os.sep+"create")
+    properties.setProperty("alter.dir",Path.path(properties.getPropertyValue("current.dir"),"alter"))
+    properties.setProperty("create.dir",Path.path(properties.getPropertyValue("current.dir"),"create"))
     properties.setProperty("project.file", "myproject.conf")
 
 
@@ -50,8 +51,53 @@ class TestBase(unittest.TestCase):
     parser.checkRequiredOptions()
     parser.checkRequiredArguments()
     generatePlugin.execute(commandLine, properties)
+
     
-    
+  def testGenerateUpdateSpecific(self):
+    properties = Properties()    
+    properties.setProperty("noora.dir", NOORA_DIR)
+    properties.setProperty("plugin.dir", Path.path(NOORA_DIR,"org","noora","plugin"))
+    properties.setProperty("current.dir", Path.path(os.path.abspath('.'),'example'))
+    properties.setProperty("alter.dir",Path.path(properties.getPropertyValue("current.dir"),"alter"))
+    properties.setProperty("create.dir",Path.path(properties.getPropertyValue("current.dir"),"create"))
+    properties.setProperty("project.file", "myproject.conf")
+
+
+
+    #connectable=MysqlConnector()
+    generatePlugin = GeneratePlugin()
+    print generatePlugin.getRevision()
+    options = generatePlugin.getOptions(properties)
+
+    arguments = ['-v=1.0.6']
+    parser = Parser()
+    commandLine = parser.parse(options,arguments)
+    parser.checkRequiredOptions()
+    parser.checkRequiredArguments()
+    generatePlugin.execute(commandLine, properties)    
+
+  def testGenerateUpdateGuess(self):
+    properties = Properties()    
+    properties.setProperty("noora.dir", NOORA_DIR)
+    properties.setProperty("plugin.dir", Path.path(NOORA_DIR,"org","noora","plugin"))
+    properties.setProperty("current.dir", Path.path(os.path.abspath('.'),'example'))
+    properties.setProperty("alter.dir",Path.path(properties.getPropertyValue("current.dir"),"alter"))
+    properties.setProperty("create.dir",Path.path(properties.getPropertyValue("current.dir"),"create"))
+    properties.setProperty("project.file", "myproject.conf")
+
+
+
+    #connectable=MysqlConnector()
+    generatePlugin = GeneratePlugin()
+    print generatePlugin.getRevision()
+    options = generatePlugin.getOptions(properties)
+
+    arguments = []
+    parser = Parser()
+    commandLine = parser.parse(options,arguments)
+    parser.checkRequiredOptions()
+    parser.checkRequiredArguments()
+    generatePlugin.execute(commandLine, properties)    
        
 
 if __name__=='__main__':
