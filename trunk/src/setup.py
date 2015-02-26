46ddf850857b8ce50bfff07ae16fc75271c8dd3c
janripke <janripke@b27cb4f7-efab-4f3b-aaa8-6348f188646f>
2015-02-26 19:03:20 +0000

    typo in version

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='noora',
-    version='1.0.0',
+    version='1.0.1',
     description='noora database project development.',
     author='Jan Ripke',
     author_email='janripke@gmail.com',
