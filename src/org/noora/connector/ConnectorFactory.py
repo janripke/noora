e2d51239813fae4a3b623bb8c0e4c7da00ab3dc5
janripke <34332389+janripke@users.noreply.github.com>
2017-08-17 12:59:46 +0000

    import to implementation

--- src/org/noora/connector/ConnectorFactory.py
+++ src/org/noora/connector/ConnectorFactory.py
@@ -1,24 +1,23 @@
-from org.noora.connector.MysqlConnector import MysqlConnector
-from org.noora.connector.OracleConnector import OracleConnector
-from org.noora.connector.PostgresqlConnector import PostgresqlConnector
-from org.noora.connector.TripolisDeployEmailConnector import TripolisDeployEmailConnector
-
 class ConnectorFactory:
   def __init__(self):
     pass
   
   @staticmethod
   def newMysqlConnector():
+    from org.noora.connector.MysqlConnector import MysqlConnector
     return MysqlConnector() 
   
   @staticmethod
   def newOracleConnector():
+    from org.noora.connector.OracleConnector import OracleConnector
     return OracleConnector() 
 
   @staticmethod
   def newPostgresqlConnector():
+    from org.noora.connector.PostgresqlConnector import PostgresqlConnector
     return PostgresqlConnector()
 
   @staticmethod
   def newTripolisDeployEmailConnector():
+    from org.noora.connector.TripolisDeployEmailConnector import TripolisDeployEmailConnector
     return TripolisDeployEmailConnector()
