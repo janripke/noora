f34a27dfb3cf43f9c764fcb3041931c82b476bbe
Niels Roosen <niels@okkernoot.net>
2019-02-12 15:32:35 +0100

    Make PropertyHelper a new style class, add docstrings to all methods

--- noora/system/PropertyHelper.py
+++ noora/system/PropertyHelper.py
@@ -2,11 +2,13 @@ import os
 import json
 
 
-class PropertyHelper:
-    def __init__(self):
-        pass
+class PropertyHelper(object):
+    """
+    Helper class providing various database convenience methods.
+    """
 
     @staticmethod
+    # FIXME: write a proper docstring
     def get_database_folder(database, folder_aliases):
         if folder_aliases:
             for folder_alias in folder_aliases:
@@ -17,6 +19,15 @@ class PropertyHelper:
 
     @staticmethod
     def get_mssql_user(users, host, schema):
+        """
+        For a list of host/schema/user combinations, return the username that matches the host
+        and schema.
+
+        :param users: list containing a sequence of AT LEAST (host, schema, username)
+        :param host: The hostname to match
+        :param schema: The schema name to match
+        :return: A matched username or None
+        """
         for user in users:
             if user[0].lower() == host.lower() and user[1].lower() == schema.lower():
                 return user[2]
@@ -24,6 +35,15 @@ class PropertyHelper:
 
     @staticmethod
     def get_mssql_password(users, host, schema):
+        """
+        For a list of host/schema/user/password combinations, return the password that matches the
+        host, schema and user.
+
+        :param users: list containing a sequence of AT LEAST (host, schema, username, password)
+        :param host: The hostname to match
+        :param schema: The schema name to match
+        :return: A password or None
+        """
         for user in users:
             if user[0].lower() == host.lower() and user[1].lower() == schema.lower():
                 return user[3]
@@ -31,6 +51,15 @@ class PropertyHelper:
 
     @staticmethod
     def get_mysql_user(users, host, database):
+        """
+        For a list of host/database/user combinations, return the username that matches the
+        host, database and user.
+
+        :param users: list containing a sequence of AT LEAST (host, database, username)
+        :param host: The hostname to match
+        :param database: The database to match
+        :return: A username or None
+        """
         for user in users:
             if user[0].lower() == host.lower() and user[1].lower() == database.lower():
                 return user[2]
@@ -38,6 +67,15 @@ class PropertyHelper:
 
     @staticmethod
     def get_mysql_passwd(users, host, database):
+        """
+        For a list of host/database/user/password combinations, return the password that matches the
+        host, database and user.
+
+        :param users: list containing a sequence of AT LEAST (host, database, username, password)
+        :param host: The hostname to match
+        :param database: The database to match
+        :return: A password or None
+        """
         for user in users:
             if user[0].lower() == host.lower() and user[1].lower() == database.lower():
                 return user[3]
@@ -45,13 +83,33 @@ class PropertyHelper:
 
     @staticmethod
     def get_postgres_user(users, host, port, database):
+        """
+        For a list of host/port/database/user combinations, return the username that matches the
+        host, port, database and user.
+
+        :param users: list containing a sequence of AT LEAST (host, port, database, username)
+        :param host: The hostname to match
+        :param port: The port to match
+        :param database: The database to match
+        :return: A username or None
+        """
         for user in users:
-            if user[0].lower() == host.lower() and user[1].lower() == port.lower() and user[2].lower() == database.lower():
+            if user[0].lower() == host.lower() and user[1].lower() == port.lower() and \
+                    user[2].lower() == database.lower():
                 return user[3]
         return None
 
     @staticmethod
     def get_oracle_user(users, sid, scheme):
+        """
+        For a list of sid/scheme/user combinations, return the username that matches the
+        sid, scheme and user.
+
+        :param users: list containing a sequence of AT LEAST (sid, scheme, username)
+        :param sid: The Site Identifier (SID) to match
+        :param scheme: The scheme to match
+        :return: A username or None
+        """
         for user in users:
             if user[0].lower() == sid.lower() and user[1].lower() == scheme.lower():
                 return user[2]
@@ -59,6 +117,15 @@ class PropertyHelper:
 
     @staticmethod
     def get_oracle_passwd(users, sid, scheme):
+        """
+        For a list of sid/scheme/user/password combinations, return the password that matches the
+        sid, scheme and user.
+
+        :param users: list containing a sequence of AT LEAST (sid, scheme, username, password)
+        :param sid: The Site Identifier (SID) to match
+        :param scheme: The scheme to match
+        :return: A password or None
+        """
         for user in users:
             if user[0].lower() == sid.lower() and user[1].lower() == scheme.lower():
                 return user[3]
@@ -66,7 +133,9 @@ class PropertyHelper:
 
     @staticmethod
     def get_profile(properties):
-        # retrieve the profile of database project
+        """
+        Retrieve the profile for the database project in the properties.
+        """
         profile = None
         project = properties.get('project')
         if project:
