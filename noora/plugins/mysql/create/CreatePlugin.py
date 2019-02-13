a8fc1d7e3a464250817b1643a9911de2467f5875
Niels Roosen <niels@okkernoot.net>
2019-02-13 19:16:27 +0100

    Reorder imports, pep-8 compliance, object reference shadowing

--- noora/plugins/mysql/create/CreatePlugin.py
+++ noora/plugins/mysql/create/CreatePlugin.py
@@ -1,9 +1,12 @@
 import os
-from noora.plugins.Plugin import Plugin
-from noora.plugins.Fail import Fail
+
 from noora.system.PropertyHelper import PropertyHelper
 from noora.system.Ora import Ora
 from noora.io.File import File
+
+from noora.plugins.Plugin import Plugin
+from noora.plugins.Fail import Fail
+
 from noora.connectors.MysqlConnector import MysqlConnector
 from noora.connectors.ConnectionExecutor import ConnectionExecutor
 
@@ -40,7 +43,8 @@ class CreatePlugin(Plugin):
         database_aliases = properties.get('database_aliases')
         Fail.fail_on_invalid_alias(alias, properties)
 
-        # if an alias is given, only this database will be installed, other databases will be ignored.
+        # if an alias is given, only this database will be installed, other databases will be
+        # ignored.
         if alias:
             print("using alias :" + alias)
             databases = [alias]
@@ -48,41 +52,46 @@ class CreatePlugin(Plugin):
         # retrieve the user credentials for this database project.
         users = properties.get('mysql_users')
 
-        # try to retrieve the users from the credentials file, when no users are configured in myproject.json.
+        # try to retrieve the users from the credentials file, when no users are configured in
+        # myproject.json.
         if not users:
             # retrieve the name of this database project, introduced in version 1.0.12
             profile = PropertyHelper.get_profile(properties)
             if profile:
                 users = profile.get('mysql_users')
 
-        # fail when no users are found. This means that they are not set in myproject.json or credentials.json
+        # fail when no users are found. This means that they are not set in myproject.json or
+        # credentials.json
         Fail.fail_on_no_users(users)
 
         connector = self.get_connector()
         create_dir = properties.get('create.dir')
 
         for database in databases:
-            print("creating database '" + database + "' on host '" + host + "' using environment '" + environment + "'")
+            print("creating database '" + database +
+                  "' on host '" + host +
+                  "' using environment '" + environment + "'")
 
             username = PropertyHelper.get_mysql_user(users, host, database)
             password = PropertyHelper.get_mysql_passwd(users, host, database)
 
-            executor = dict()
-            executor['host'] = host
-            executor['database'] = database
-            executor['username'] = username
-            executor['password'] = password
+            executor = {
+                'host': host,
+                'database': database,
+                'username': username,
+                'password': password,
+            }
 
             database_folder = PropertyHelper.get_database_folder(database, database_aliases)
 
-            for object in objects:
+            for obj in objects:
                 # global ddl objects
 
-                folder = File(os.path.join(create_dir, database_folder, 'ddl', object))
+                folder = File(os.path.join(create_dir, database_folder, 'ddl', obj))
                 ConnectionExecutor.execute(connector, executor, properties, folder)
 
                 # environment specific ddl objects
-                folder = File(os.path.join(create_dir, database_folder, 'ddl', object, environment))
+                folder = File(os.path.join(create_dir, database_folder, 'ddl', obj, environment))
                 ConnectionExecutor.execute(connector, executor, properties, folder)
 
             # global dat objects
