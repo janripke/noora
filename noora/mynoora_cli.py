4ec948ba4226ca6da98413ebe8f53c963edf03c7
Niels Roosen <niels@okkernoot.net>
2019-03-07 22:06:39 +0100

    Add some help text

--- noora/mynoora_cli.py
+++ noora/mynoora_cli.py
@@ -7,8 +7,11 @@ from noora.system.App import App
 
 
 @click.command(cls=App, invoke_without_command=True, no_args_is_help=True)
-@click.option('-r', '--revision', is_flag=True)
+@click.option('-r', '--revision', is_flag=True, help="Report the version of Noora and exit.")
 def main(revision):
+    """
+    NoOra is a database deployment tool used to automate the database deployment cycle.
+    """
     # show the revision
     if revision:
         print(noora.__title__ + " version " + noora.__version__)
