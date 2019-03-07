2cff7aa55f805c251244bb885dba3c194ede561a
Niels Roosen <niels@okkernoot.net>
2019-03-07 22:09:40 +0100

    Big rewrite of generate plugins to split them up into create and update methods with generic process method. MSSQL is refactored, MySQL is WIP

--- noora/plugins/mysql/generate/GeneratePlugin.py
+++ noora/plugins/mysql/generate/GeneratePlugin.py
@@ -14,129 +14,150 @@ from noora.io.File import File
 from noora.io.Files import Files
 
 
-@click.command()
-@click.option('-v', '--version')
-def GeneratePlugin(version):
-    project = properties.get('project')
+class GeneratePlugin(object):
+    @staticmethod
+    @click.command()
+    @click.option('-h', '--host', required=False, prompt=True, default='localhost')
+    @click.option('-p', '--port', required=False, prompt=True, type=int, default=3306)
+    @click.option('-d', '--database', required=True, prompt='Database name')
+    @click.option('-U', '--username', required=True, prompt='Database username')
+    @click.option('-P', '--password', required=True, prompt='Database password',
+                  hide_input=True, confirmation_prompt=True)
+    @click.option('-v', '--version', required=False,
+                  prompt='Initial project version', default='1.0.0')
+    def create_project(host, port, database, schema, user, password, version):
+        """
+        Generate a new MySQL database project
+        """
+        pass
 
-    current_dir = properties.get('current.dir')
-    project_file = properties.get('project.file')
-    config_file = File(os.path.join(current_dir, project_file))
-    template_dir = os.path.join(
-        properties.get('plugin.dir'), 'mysql', 'generate', 'templates')
-    if not config_file.exists():
-        database = input('database : ')
-        project = database + "-db"
-        host = input('host [localhost] : ')
-        host = Ora.nvl(host, "localhost")
-        port = input('port [3306] : ') or '3306'
-        username = input('username : ')
-        password = input('password : ')
-        version = input('version [1.0.0]: ')
-        version = Ora.nvl(version, "1.0.0")
+    @staticmethod
+    @click.command()
+    @click.option('-v', '--version')
+    def upgrade_project(version):
+        """
+        Bootstrap a new version of a MySQL database project
+        """
+        project = properties.get('project')
 
-        os.mkdir(project)
-        template_file = os.path.join(template_dir, project_file)
+        current_dir = properties.get('current.dir')
+        project_file = properties.get('project.file')
+        config_file = File(os.path.join(current_dir, project_file))
+        template_dir = os.path.join(
+            properties.get('plugin.dir'), 'mysql', 'generate', 'templates')
+        if not config_file.exists():
+            database = input('database : ')
+            project = database + "-db"
+            host = input('host [localhost] : ')
+            host = Ora.nvl(host, "localhost")
+            port = input('port [3306] : ') or '3306'
+            username = input('username : ')
+            password = input('password : ')
+            version = input('version [1.0.0]: ')
+            version = Ora.nvl(version, "1.0.0")
 
-        f = open(template_file)
-        stream = f.read()
-        f.close()
-        stream = stream.replace('{project}', project)
-        stream = stream.replace('{database}', database)
-        stream = stream.replace('{host}', host)
-        stream = stream.replace('{port}', port)
-        stream = stream.replace('{username}', username)
-        stream = stream.replace('{password}', password)
-        stream = stream.replace('{version}', version)
+            os.mkdir(project)
+            template_file = os.path.join(template_dir, project_file)
 
-        config_file = os.path.join(current_dir, project, project_file)
-        f = open(config_file, 'w')
-        f.write(stream)
-        f.close()
+            f = open(template_file)
+            stream = f.read()
+            f.close()
+            stream = stream.replace('{project}', project)
+            stream = stream.replace('{database}', database)
+            stream = stream.replace('{host}', host)
+            stream = stream.replace('{port}', port)
+            stream = stream.replace('{username}', username)
+            stream = stream.replace('{password}', password)
+            stream = stream.replace('{version}', version)
 
-    properties['alter.dir'] = os.path.join(current_dir, project, 'alter')
-    properties['create.dir'] = os.path.join(current_dir, project, 'create')
-
-    config_file = os.path.join(current_dir, project, project_file)
-    f = open(config_file)
-    data = json.load(f)
-    for key in data.keys():
-        properties[key] = data[key]
-    f.close()
-
-    versions = Versions()
-    version_loader = VersionLoader(versions)
-    version_loader.load(properties)
-    versions.sort()
-    version_guesser = VersionGuesser(properties, versions)
-    next_version = version_guesser.guess(version).to_string()
-    version_dir = version_guesser.to_folder(next_version)
-
-    # create the version folder
-    os.makedirs(version_dir)
-
-    databases = properties.get('databases')
-    version_database = properties.get('version_database')
-    default_version = properties.get('default_version')
-    environments = properties.get('environments')
-    objects = properties.get('create_objects')
-
-    for database in databases:
-        # create the scheme folder
-        database_dir = os.path.join(version_dir, database)
-        os.mkdir(database_dir)
-
-        # create the dat folder
-        dat_dir = os.path.join(database_dir, 'dat')
-        os.mkdir(dat_dir)
-
-        # create the version script in the dat folder
-        if database == version_database:
-            version_file = os.path.join(dat_dir, 'version.sql')
-            f = open(version_file, 'w')
-
-            if next_version == default_version:
-                stream = properties.get('version_insert_statement')
-            else:
-                stream = properties.get('version_update_statement')
-
-            stream = stream.replace('<version>', next_version)
+            config_file = os.path.join(current_dir, project, project_file)
+            f = open(config_file, 'w')
             f.write(stream)
             f.close()
 
-            # sqlScript=self.getSqlVersionStatement(versions, version)
-            # projectHelper.writeFile(datFolder+os.sep+'version.sql', sqlScript)
+        properties['alter.dir'] = os.path.join(current_dir, project, 'alter')
+        properties['create.dir'] = os.path.join(current_dir, project, 'create')
 
-        # create the environment folders in the dat folder
-        for environment in environments:
-            os.mkdir(os.path.join(dat_dir, environment))
+        config_file = os.path.join(current_dir, project, project_file)
+        f = open(config_file)
+        data = json.load(f)
+        for key in data.keys():
+            properties[key] = data[key]
+        f.close()
 
-            # create the environment script in the dat folder.
-            if database == version_database and next_version == default_version:
-                environment_file = os.path.join(dat_dir, environment, 'environment.sql')
+        versions = Versions()
+        version_loader = VersionLoader(versions)
+        version_loader.load(properties)
+        versions.sort()
+        version_guesser = VersionGuesser(properties, versions)
+        next_version = version_guesser.guess(version).to_string()
+        version_dir = version_guesser.to_folder(next_version)
 
-                f = open(environment_file, 'w')
-                stream = properties.get('environment_insert_statement')
-                stream = stream.replace('<environment>', environment)
+        # create the version folder
+        os.makedirs(version_dir)
+
+        databases = properties.get('databases')
+        version_database = properties.get('version_database')
+        default_version = properties.get('default_version')
+        environments = properties.get('environments')
+        objects = properties.get('create_objects')
+
+        for database in databases:
+            # create the scheme folder
+            database_dir = os.path.join(version_dir, database)
+            os.mkdir(database_dir)
+
+            # create the dat folder
+            dat_dir = os.path.join(database_dir, 'dat')
+            os.mkdir(dat_dir)
+
+            # create the version script in the dat folder
+            if database == version_database:
+                version_file = os.path.join(dat_dir, 'version.sql')
+                f = open(version_file, 'w')
+
+                if next_version == default_version:
+                    stream = properties.get('version_insert_statement')
+                else:
+                    stream = properties.get('version_update_statement')
+
+                stream = stream.replace('<version>', next_version)
                 f.write(stream)
                 f.close()
 
-        # create the ddl folder
-        ddl_dir = os.path.join(database_dir, 'ddl')
-        os.mkdir(ddl_dir)
+                # sqlScript=self.getSqlVersionStatement(versions, version)
+                # projectHelper.writeFile(datFolder+os.sep+'version.sql', sqlScript)
 
-        # create the object folders in the ddl folder
-        for obj in objects:
-            os.mkdir(os.path.join(ddl_dir, obj))
+            # create the environment folders in the dat folder
+            for environment in environments:
+                os.mkdir(os.path.join(dat_dir, environment))
 
-        # create the template code on create.
-        if database == version_database and next_version == default_version:
+                # create the environment script in the dat folder.
+                if database == version_database and next_version == default_version:
+                    environment_file = os.path.join(dat_dir, environment, 'environment.sql')
+
+                    f = open(environment_file, 'w')
+                    stream = properties.get('environment_insert_statement')
+                    stream = stream.replace('<environment>', environment)
+                    f.write(stream)
+                    f.close()
+
+            # create the ddl folder
+            ddl_dir = os.path.join(database_dir, 'ddl')
+            os.mkdir(ddl_dir)
+
+            # create the object folders in the ddl folder
             for obj in objects:
-                object_dir = os.path.join(template_dir, obj)
-                if os.path.exists(object_dir):
-                    files = Files.list(File(object_dir))
-                    for file in files:
-                        shutil.copyfile(
-                            file.get_url(), os.path.join(ddl_dir, obj, file.tail()))
+                os.mkdir(os.path.join(ddl_dir, obj))
 
-    print("version {} created.".format(next_version))
+            # create the template code on create.
+            if database == version_database and next_version == default_version:
+                for obj in objects:
+                    object_dir = os.path.join(template_dir, obj)
+                    if os.path.exists(object_dir):
+                        files = Files.list(File(object_dir))
+                        for file in files:
+                            shutil.copyfile(
+                                file.get_url(), os.path.join(ddl_dir, obj, file.tail()))
+
+        print("version {} created.".format(next_version))
