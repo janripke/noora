04599cb4a8727522ac11aa4a84797c37b6303c21
Niels Roosen <niels@okkernoot.net>
2019-03-08 16:44:28 +0100

    Change plugin scanning mechanism

--- noora/system/App.py
+++ noora/system/App.py
@@ -4,6 +4,7 @@ from importlib import import_module
 import click
 
 from noora.system.Properties import properties
+from noora.plugins.Plugin import Plugin
 
 
 class App(click.MultiCommand):
@@ -12,19 +13,44 @@ class App(click.MultiCommand):
         Get the commands based on the context. If 'project' is defined on the
         properties, look up all available plugins for the project's
         technology. Else, only return 'generate'
-        :param ctx:
-        :return:
+
+        :return: A list of available commands
         """
         if 'project' in properties:
-            return ["generate", ]
+            technology = properties.get('technology')
+            res = []
+
+            # Loop over the plugins for this technology and add all plugins to the list that have
+            # a valid execute method # configured.
+            plugin_path = os.path.join(properties['plugin.dir'], technology)
+            for p in os.listdir(plugin_path):
+                p_path = os.path.join(plugin_path, p)
+                if os.path.isdir(p_path):
+                    for f in os.listdir(p_path):
+                        f_path = os.path.join(p_path, f)
+                        if os.path.isfile(f_path) and f.endswith('Plugin.py'):
+                            pname = f[:-3]
+                            # Try importing the module and if the module has a class with the same name
+                            mod = import_module('noora.plugins.{}.{}.{}'.format(technology, p, pname))
+                            cls = getattr(mod, pname)
+                            if cls and issubclass(cls, Plugin) and cls.get_executor():
+                                res.append(p)
+            return res
+
+        # NOTE: in the future it might happen we support more than just 'generate' outside of a project's scope.
+        # In this case one should loop over all technologies and plugins per technology to check if an execute method\
+        # exists that can be run outside of a project's scope.
         else:
             return ['generate', ]
 
     def get_command(self, ctx, cmd_name):
         if 'project' in properties:
-            mod = import_module('noora.plugins.{}.generate.GeneratePlugin'.format(
-                properties['technology']))
-            return mod.GeneratePlugin.upgrade_project
+            # FIXME: not very nice to use cmd_name to generate plugin module name
+            cls_name = "{}Plugin".format(cmd_name.capitalize())
+            mod = import_module('noora.plugins.{}.{}.{}'.format(
+                properties['technology'], cmd_name, cls_name))
+            cls = getattr(mod, cls_name)
+            return cls.get_executor()
         else:
             mod = import_module('noora.plugins.GenerateProject')
             return mod.cli
