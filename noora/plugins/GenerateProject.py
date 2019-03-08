5ed55ecf8d6815af61fe2b419f35d99838e72b9f
Niels Roosen <niels@okkernoot.net>
2019-03-08 16:44:28 +0100

    Change plugin scanning mechanism

--- noora/plugins/GenerateProject.py
+++ noora/plugins/GenerateProject.py
@@ -14,13 +14,13 @@ class GenerateProject(click.MultiCommand):
         cur_path = os.path.dirname(__file__)
         for f in os.listdir(cur_path):
             f_path = "{}/{}".format(cur_path, f)
-            if os.path.isdir(f_path) and os.path.exists("{}/generate/GenerateProject.py".format(f_path)):
+            if os.path.isdir(f_path) and os.path.exists("{}/generate/GeneratePlugin.py".format(f_path)):
                 res.append(f)
         return res
 
     def get_command(self, ctx, cmd_name):
         mod = import_module("noora.plugins.{}.generate.GeneratePlugin".format(cmd_name))
-        return mod.GeneratePlugin.create_project
+        return mod.GeneratePlugin.get_executor(outside_scope=True)
 
 
 @click.command(cls=GenerateProject)
