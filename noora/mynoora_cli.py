ff6bad9f9b36e8f7033f8a1ce4c2e512dcc55bb3
Niels Roosen <niels@okkernoot.net>
2019-03-11 15:28:31 +0100

    Add explicit exit to revision condition.

--- noora/mynoora_cli.py
+++ noora/mynoora_cli.py
@@ -8,14 +8,15 @@ from noora.system.App import App
 
 @click.command(cls=App, invoke_without_command=True, no_args_is_help=True)
 @click.option('-r', '--revision', is_flag=True, help="Report the version of Noora and exit.")
-def main(revision):
+@click.pass_context
+def main(ctx, revision):
     """
     NoOra is a database deployment tool used to automate the database deployment cycle.
     """
     # show the revision
     if revision:
         print(noora.__title__ + " version " + noora.__version__)
-        return
+        ctx.exit()
 
 
 if __name__ == "__main__":
