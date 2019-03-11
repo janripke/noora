ee9be7be2712fea9795141b4b2bcaa632699a213
Niels Roosen <niels@okkernoot.net>
2019-03-11 15:28:50 +0100

    Rewrite App to initialize properties instead of using a global symbol, so we can pass it around in the context. Also prepare the way to unplug the cli code from the Plugin code

--- noora/system/App.py
+++ noora/system/App.py
@@ -3,38 +3,44 @@ from importlib import import_module
 
 import click
 
-from noora.system.Properties import properties
-from noora.plugins.Plugin import Plugin
+from noora.system.Properties import Properties
 
 
 class App(click.MultiCommand):
+    def initialize_and_get_properties(self, ctx):
+        """
+        Makes sure the Properties object is initialized and assigned to the context object.
+        :param ctx: The click context
+        :return: a reference to the properties object
+        """
+        if not ctx.obj:
+            ctx.obj = Properties()
+        return ctx.obj
+
     def list_commands(self, ctx):
         """
         Get the commands based on the context. If 'project' is defined on the
         properties, look up all available plugins for the project's
-        technology. Else, only return 'generate'
+        technology. Else, only return 'generate'.
 
         :return: A list of available commands
         """
-        if 'project' in properties:
-            technology = properties.get('technology')
+        props = self.initialize_and_get_properties()
+
+        if 'project' in props:
+            technology = props.get('technology')
             res = []
 
             # Loop over the plugins for this technology and add all plugins to the list that have
             # a valid execute method # configured.
-            plugin_path = os.path.join(properties['plugin.dir'], technology)
+            plugin_path = os.path.join(props['plugin.dir'], technology)
             for p in os.listdir(plugin_path):
                 p_path = os.path.join(plugin_path, p)
                 if os.path.isdir(p_path):
-                    for f in os.listdir(p_path):
-                        f_path = os.path.join(p_path, f)
-                        if os.path.isfile(f_path) and f.endswith('Plugin.py'):
-                            pname = f[:-3]
-                            # Try importing the module and if the module has a class with the same name
-                            mod = import_module('noora.plugins.{}.{}.{}'.format(technology, p, pname))
-                            cls = getattr(mod, pname)
-                            if cls and issubclass(cls, Plugin) and cls.get_executor():
-                                res.append(p)
+                    # Try to import the cli command from the package root
+                    mod = import_module('noora.plugins.{}.{}'.format(technology, p))
+                    if getattr(mod, 'cli'):
+                        res.append(p)
             return res
 
         # NOTE: in the future it might happen we support more than just 'generate' outside of a project's scope.
@@ -44,13 +50,14 @@ class App(click.MultiCommand):
             return ['generate', ]
 
     def get_command(self, ctx, cmd_name):
-        if 'project' in properties:
-            # FIXME: not very nice to use cmd_name to generate plugin module name
-            cls_name = "{}Plugin".format(cmd_name.capitalize())
-            mod = import_module('noora.plugins.{}.{}.{}'.format(
-                properties['technology'], cmd_name, cls_name))
-            cls = getattr(mod, cls_name)
-            return cls.get_executor()
-        else:
+        props = self.initialize_and_get_properties(ctx)
+
+        if 'project' in props:
+            # Import the module and return the cli method
+            mod = import_module('noora.plugins.{}.{}'.format(props['technology'], cmd_name))
+            return mod.cli
+        elif cmd_name == 'generate':
             mod = import_module('noora.plugins.GeneratePlugin')
             return mod.cli
+        else:
+            raise click.Abort("critical: unknown command: {}".format(cmd_name))
