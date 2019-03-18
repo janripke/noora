b5c24e3d68e790dc52e75ec8e666fd9b5669f37b
Niels Roosen <niels@okkernoot.net>
2019-03-18 16:34:10 +0100

    Change parameters to be non-interactive for non-generate commands, so commands can fail when used in an automated context

--- noora/plugins/mysql/drop/__init__.py
+++ noora/plugins/mysql/drop/__init__.py
@@ -4,10 +4,10 @@ from .DropPlugin import DropPlugin
 
 
 @click.command()
-@click.option('-h', '--host', required=True, prompt=True, default='localhost')
-@click.option('-d', '--database', required=False, prompt='Database name')
-@click.option('-s', '--environment', required=False, prompt='Environment')
-@click.option('-a', '--alias', required=False, prompt='Alias. Overrules the database option')
+@click.option('-h', '--host', required=True, default='localhost')
+@click.option('-d', '--database', required=False, help='Database name')
+@click.option('-s', '--environment', required=False, help='Environment')
+@click.option('-a', '--alias', required=False, help='Alias. Overrules the database option')
 @click.pass_obj
 def cli(props, host, database, environment, alias):
     """
--- noora/plugins/mysql/update/__init__.py
+++ noora/plugins/mysql/update/__init__.py
@@ -5,10 +5,10 @@ from .UpdatePlugin import UpdatePlugin
 
 @click.command()
 @click.option('-v', '--version', required=False)
-@click.option('-h', '--host', required=True, prompt=True, default='localhost')
-@click.option('-s', '--database', required=False, prompt='Database name')
-@click.option('-s', '--environment', required=False, prompt='Environment')
-@click.option('-a', '--alias', required=False, prompt='Alias. Overrules the database option')
+@click.option('-h', '--host', required=True, default='localhost')
+@click.option('-s', '--database', required=False, help='Database name')
+@click.option('-s', '--environment', required=False, help='Environment')
+@click.option('-a', '--alias', required=False, help='Alias. Overrules the database option')
 @click.pass_obj
 def cli(props, version, host, database, environment, alias):
     """
