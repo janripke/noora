bfaaf3e169feb58f059cfd578d5a968c5b66a4c1
Niels Roosen <niels@okkernoot.net>
2019-03-26 16:32:33 +0100

    Remove prompting for non-generate commands

--- noora/plugins/mssql/update/__init__.py
+++ noora/plugins/mssql/update/__init__.py
@@ -5,9 +5,9 @@ from .UpdatePlugin import UpdatePlugin
 
 @click.command()
 @click.option('-v', '--version', required=False)
-@click.option('-h', '--host', required=True, prompt=True, default='localhost')
-@click.option('-s', '--schema', required=False, prompt='Schema name')
-@click.option('-s', '--environment', required=False, prompt='Environment')
+@click.option('-h', '--host', required=True, default='localhost')
+@click.option('-s', '--schema', required=False, help='Schema name')
+@click.option('-s', '--environment', required=False, help='Environment')
 @click.pass_obj
 def cli(props, version, host, schema, environment):
     """
