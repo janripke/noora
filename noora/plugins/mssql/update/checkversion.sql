2c56e149b04742b0fc1816bdd3655c72d1087259
Niels Roosen <niels@okkernoot.net>
2019-03-26 16:33:18 +0100

    Make checkversion report the current version

--- noora/plugins/mssql/update/checkversion.sql
+++ noora/plugins/mssql/update/checkversion.sql
@@ -2,11 +2,11 @@ begin
 declare @result varchar(4000)
     set @result = (select value
                    from   application_properties
-                   where  name = 'application.version'
-				   and    value = '{previous}')
-	if @result is null
+                   where  name = 'application.version')
+	if @result != '{previous}'
 	begin
-		declare @message varchar(255) = 'incorrect database version, the found version is ' + '{previous}'
+		declare @message varchar(255) = 'incorrect database version, current: ' + @result +
+		                                ', expected: {previous}'
 		RAISERROR (@message,  18, 20)
 	end
 end
