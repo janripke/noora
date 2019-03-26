0665eda6b247de989cc495fa2626446778ce4336
Niels Roosen <niels@okkernoot.net>
2019-03-26 16:32:33 +0100

    Remove prompting for non-generate commands

--- noora/plugins/mssql/drop/__init__.py
+++ noora/plugins/mssql/drop/__init__.py
@@ -4,9 +4,9 @@ from .DropPlugin import DropPlugin
 
 
 @click.command()
-@click.option('-h', '--host', required=True, prompt=True, default='localhost')
-@click.option('-s', '--schema', required=False, prompt='Schema name')
-@click.option('-s', '--environment', required=False, prompt='Environment')
+@click.option('-h', '--host', required=True, default='localhost')
+@click.option('-s', '--schema', required=False, help='Schema name')
+@click.option('-s', '--environment', required=False, help='Environment')
 @click.pass_obj
 def cli(props, host, schema, environment):
     """
