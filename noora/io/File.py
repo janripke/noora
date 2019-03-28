33e298cf398c4cd479783d3125b56aff79d59eb8
Niels Roosen <niels@okkernoot.net>
2019-03-28 11:52:21 +0100

    More docstrings

--- noora/io/File.py
+++ noora/io/File.py
@@ -2,6 +2,9 @@ import os
 
 
 class File(object):
+    """
+    File management functionality. Initialize a file object with its path (url).
+    """
     def __init__(self, url=None):
         self.__url = url
 
@@ -9,29 +12,48 @@ class File(object):
         return self.__url
 
     def is_file(self):
+        """
+        Returns True if url points to a file, false if not (e.g. when the
+        file is a directory).
+        """
         url = self.get_url()
         return os.path.isfile(url)
 
     def is_dir(self):
+        """
+        Returns True if url points to a directory, False otherwise.
+        """
         url = self.get_url()
         return os.path.isdir(url)
 
     def tail(self):
+        """
+        Returns the tail part after splitting the File's url.
+        """
         url = self.get_url()
         head, tail = os.path.split(url)
         return tail
 
     def head(self):
+        """
+        Returns the head part after splitting the File's url.
+        """
         url = self.get_url()
         head, tail = os.path.split(url)
         return head
 
     def extension(self):
+        """
+        Get the file's extension
+        """
         url = self.get_url()
         filename, extension = os.path.splitext(url)
         return extension
 
     def exists(self):
+        """
+        Check if the file exists.
+        """
         if self.is_file():
             return True
         if self.is_dir():
