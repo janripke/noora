d25153318e5812fdce7596e1f8eaa049c9cf337b
Niels Roosen <niels@okkernoot.net>
2019-04-03 15:43:44 +0200

    Generate plugin for postgres implemented

--- noora/plugins/postgresql/generate/templates/trg/application_properties_bu.sql
+++ noora/plugins/postgresql/generate/templates/trg/application_properties_bu.sql
@@ -1,12 +1,3 @@
-delimiter |
-
 CREATE TRIGGER application_properties_bu
   BEFORE UPDATE ON application_properties
-    FOR EACH ROW BEGIN
-      SET NEW.updated_at=NOW();
-      SET NEW.updated_by=CURRENT_USER();
-    END;
-|
-
-delimiter ;
-
+  EXECUTE PROCEDURE application_properties_bu_fct
