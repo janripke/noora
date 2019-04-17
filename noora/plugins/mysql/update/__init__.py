82b725628c12b710044ffe1e60aa5f7afdeb5a12
Niels Roosen <niels@okkernoot.net>
2019-04-17 22:25:03 +0200

    Fix imports and command line options

--- noora/plugins/mysql/update/__init__.py
+++ noora/plugins/mysql/update/__init__.py
@@ -6,7 +6,7 @@ from .UpdatePlugin import UpdatePlugin
 @click.command()
 @click.option('-v', '--version', required=False)
 @click.option('-h', '--host', required=True, default='localhost')
-@click.option('-s', '--database', required=False, help='Database name')
+@click.option('-d', '--database', required=False, help='Database name')
 @click.option('-e', '--environment', required=False, help='Environment')
 @click.option('-a', '--alias', required=False, help='Alias. Overrules the database option')
 @click.pass_obj
