291ad39dfb1866e713d427c1a2157f7531ef549d
jan.ripke@rabobank.nl <jan.ripke@rabobank.nl>
2023-01-13 15:57:29 +0100

    invalid license information, wrong format

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Topic :: Database :: Front-Ends',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License :: 2.0',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
